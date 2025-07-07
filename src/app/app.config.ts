ba341d47a84aed1f09cb3e87789f9e8c14d0fa05
FloresRodrigo <rodrigoef25@gmail.com>
2025-07-07 16:34:45 -0300

    Flores Rodrigo - Implementacion de JWT
    Se agrego el interceptor de http para que en algunas solicitudes requiera el token de login de usuario, ahora al logearse se guarda el token en la session, y al deslogearse se elimina

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { TokenInterceptorService } from './services/token-interceptor.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true
+    },
+    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(withInterceptorsFromDi()),
+  ]
 };
